bb3a746be858b717f6b8b4963c2a205f5b64dcbc
BradBot_1 <BradBot_1@outlook.com>
2023-06-29 18:39:20 +0100

    Make Yaml.class use static methods rather than damanding an instance

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+# Objective YAML
+
+A `Java 17` GSON like approach to YAML
+
 > This project utilises [eo-yaml](https://github.com/decorators-squad/eo-yaml) internally
+
+> Serialization of an object is not supported, this simply replicates the functionality of JsonElement, JsonObject, JsonArray and JsonPrimitive from GSON for YAML
+
+## How to use
+
+Simply import `fun.bb1.yaml.Yaml` and invoke `#parseString()` with your YAML.
+
+```java
+
+import fun.bb1.yaml.Yaml;
+
+...
+
+YamlObject yaml = Yaml.parseString("example: 1", YamlObject.class);
+```
+
+You should always use `YamlObject` by default, but this method can parse other types if needed.
--- src/main/java/fun/bb1/yaml/Yaml.java
+++ src/main/java/fun/bb1/yaml/Yaml.java
@@ -16,72 +16,72 @@ import com.amihaiemil.eoyaml.YamlSequenceBuilder;
 
 public final class Yaml {
 	
-	public Yaml() {
+	private Yaml() {
 		
 	}
 	
 	@SuppressWarnings("unchecked") // can be suppressed as types are checked
-	public final <T extends IYamlElement> @NotNull T parseString(@NotNull final String yamlString, Class<T> parseType) {
+	public static final <T extends IYamlElement> @NotNull T parseString(@NotNull final String yamlString, Class<T> parseType) {
 		final YamlInput input = com.amihaiemil.eoyaml.Yaml.createYamlInput(yamlString);
 		if (parseType == YamlPrimitive.class) {
 			final Scalar scalar = handle(()->input.readPlainScalar(),
 								  handle(()->input.readFoldedBlockScalar(),
 								  handle(()->input.readLiteralBlockScalar())));
 			if (scalar == null) return (T) new YamlPrimitive("");
-			return (T) this.recurse(scalar);
+			return (T) recurse(scalar);
 		} else if (parseType == YamlObject.class) {
 			final YamlMapping mapping = handle(()->input.readYamlMapping());
 			if (mapping == null) return (T) new YamlObject();
-			return (T) this.recurse(mapping);
+			return (T) recurse(mapping);
 		} else {
 			final YamlSequence sequence = handle(()->input.readYamlSequence());
 			if (sequence == null) return (T) new YamlArray();
-			return (T) this.recurse(sequence);
+			return (T) recurse(sequence);
 		}
 	}
 	
-	private final @NotNull IYamlElement recurse(@NotNull final YamlNode yamlNode) {
+	private static final @NotNull IYamlElement recurse(@NotNull final YamlNode yamlNode) {
 		if (yamlNode instanceof Scalar scalar) return new YamlPrimitive(scalar.value()).setComment(scalar.comment().value());
 		if (yamlNode instanceof YamlMapping mapping) {
 			final YamlObject object = new YamlObject();
 			for (final YamlNode key : mapping.keys()) {
 				final YamlNode value = mapping.value(key);
-				final IYamlElement recursedKey = this.recurse(key);
-				object.add(recursedKey instanceof YamlPrimitive prim ? prim : new YamlPrimitive(recursedKey.getAsString()), this.recurse(value));
+				final IYamlElement recursedKey = recurse(key);
+				object.add(recursedKey instanceof YamlPrimitive prim ? prim : new YamlPrimitive(recursedKey.getAsString()), recurse(value));
 			}
 			return object.setComment(mapping.comment().value());
 		}
 		final YamlSequence sequence = (YamlSequence) yamlNode;
 		final YamlArray array = new YamlArray();
 		for (final YamlNode elem : sequence) {
-			array.add(this.recurse(elem));
+			array.add(recurse(elem));
 		}
 		return array.setComment(sequence.comment().value());
 	}
 	
-	public final @NotNull String toYaml(@NotNull final IYamlElement yamlElement) {
-		return this.recurse(yamlElement).toString();
+	public static final @NotNull String toYaml(@NotNull final IYamlElement yamlElement) {
+		return recurse(yamlElement).toString();
 	}
 	
-	private final @NotNull YamlNode recurse(@NotNull final IYamlElement yamlElement) {
+	private static final @NotNull YamlNode recurse(@NotNull final IYamlElement yamlElement) {
 		if (yamlElement instanceof YamlObject obj) {
 			YamlMappingBuilder objBuilder = com.amihaiemil.eoyaml.Yaml.createYamlMappingBuilder();
 			for (final YamlPrimitive objKey : obj) {
-				objBuilder = objBuilder.add(this.recurse(objKey), this.recurse(obj.get(objKey))); // JUST WHY
+				objBuilder = objBuilder.add(recurse(objKey), recurse(obj.get(objKey))); // JUST WHY
 			}
 			return yamlElement.getComment().isEmpty() ? objBuilder.build() : objBuilder.build(yamlElement.getComment());
 		}
 		if (yamlElement instanceof YamlArray arr) {
 			YamlSequenceBuilder arrBuilder = com.amihaiemil.eoyaml.Yaml.createYamlSequenceBuilder();
 			for (final IYamlElement arrElem : arr) {
-				arrBuilder = arrBuilder.add(this.recurse(arrElem)); // weird impl thing??
+				arrBuilder = arrBuilder.add(recurse(arrElem)); // weird impl thing??
 			}
 			return arr.getComment().isEmpty() ? arrBuilder.build() : arrBuilder.build(arr.getComment());
 		}
 		return buildForString(yamlElement.getAsString(), yamlElement.getComment());
 	}
 	
-	private final @NotNull Scalar buildForString(@NotNull final String str, @Nullable final String comment) {
+	private static final @NotNull Scalar buildForString(@NotNull final String str, @Nullable final String comment) {
 		final YamlScalarBuilder scalar = com.amihaiemil.eoyaml.Yaml.createYamlScalarBuilder().addLine(str);
 		if (comment == null || comment.isEmpty()) {
 			if (str.contains("\n")) return scalar.buildFoldedBlockScalar();
